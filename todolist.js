fe5209576f31324910b09b759e1c4d5dcf9f6841
AkritiSingh0720 <63656512+AkritiSingh0720@users.noreply.github.com>
2020-04-27 22:53:17 +0530

    Update todolist.js

--- todolist.js
+++ todolist.js
@@ -1,7 +1,8 @@
 
     var myNodelist = document.getElementsByTagName("LI");
     var i;
-    for (i = 0; i < myNodelist.length; i++) {
+    for (i = 0; i < myNodelist.length; i++)
+    {
       var span = document.createElement("SPAN");
       var txt = document.createTextNode("\u00D7");
       span.className = "close";
@@ -9,30 +10,36 @@
       myNodelist[i].appendChild(span);
     }
    
-    var close = document.getElementsByClassName("close");
-    var i;
-    for (i = 0; i < close.length; i++) {
-      close[i].onclick = function() {
-      var div = this.parentElement;
-      div.style.display = "none";
+     var close = document.getElementsByClassName("close");
+     var i;
+     for (i = 0; i < close.length; i++) {
+       close[i].onclick = function() {
+       var div = this.parentElement;
+       div.style.display = "none";
       }
     }
     
     var list = document.querySelector('ul');
-    list.addEventListener('click', function(ev) {
-      if (ev.target.tagName === 'LI') {
+    list.addEventListener('click', function(ev)
+    {
+      if (ev.target.tagName === 'LI') 
+      {
       ev.target.classList.toggle('checked');
       }
     }, false);
     
-    function newElement() {
+    function newElement()
+    {
       var li = document.createElement("li");
       var inputValue = document.getElementById("myInput").value;
       var t = document.createTextNode(inputValue);
       li.appendChild(t);
-      if (inputValue === '') {
+      if (inputValue === '')
+      {
       alert("You must write something!");
-      } else {
+      }
+        else
+        {
         document.getElementById("mytodo").appendChild(li);
       }
       document.getElementById("myInput").value = "";
@@ -41,15 +48,18 @@
       span.className = "close";
       span.appendChild(txt);
       li.appendChild(span);
-      for (i = 0; i < close.length; i++) {
-        close[i].onclick = function() {
+      for (i = 0; i < close.length; i++)
+      {
+        close[i].onclick = function() 
+        {
             var div = this.parentElement;
             div.style.display = "none";
         }
       }
     }
    
-    function removeAll(){
+    function removeAll()
+{
       var lst = document.getElementsByTagName("ul");
         lst[0].innerHTML = "";
     }
